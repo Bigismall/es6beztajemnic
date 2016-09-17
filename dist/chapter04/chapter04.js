33ea1ecf9f1f1fdef06e19c5b7626f0b7e9326fa
Bigismall <rondelious@gmail.com>
2016-09-17 11:12:09 +0200

    chapter 04

--- dist/chapter04/chapter04.js
+++ dist/chapter04/chapter04.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var _iterator2 = require("babel-runtime/core-js/symbol/iterator");
+
+var _iterator3 = _interopRequireDefault(_iterator2);
+
 var _getIterator2 = require("babel-runtime/core-js/get-iterator");
 
 var _getIterator3 = _interopRequireDefault(_getIterator2);
@@ -58,7 +62,7 @@ var keyString = "a string",
 sampleVariousTypeMap.set(keyString, "value associated with \"a string\"");
 sampleVariousTypeMap.set(keyObj, "value associated with keyObj");
 sampleVariousTypeMap.set(keyFunc, "value associated with keyFunc");
-logTitle("Rest parameters");
+logTitle("Rest parameters - functions");
 
 function sumAll() {
     var sum = 0;
@@ -96,6 +100,67 @@ function sumAll() {
 }
 
 log("Suma 1+2+3+4+5+6+7+8+9+10 = " + sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+logTitle("Rest parameters - arrays");
+
+var cold = ['autumn', 'winter'];
+var warm = ['spring', 'summer'];
+
+var coldAndWarm = [].concat(cold, warm);
+
+//array clone
+var coldWarmPushTest1 = [].concat(cold);
+var coldWarmPushTest2 = [].concat(cold);
+
+coldWarmPushTest1.push.apply(coldWarmPushTest1, warm);
+coldWarmPushTest2.push(warm);
+
+log(coldAndWarm);
+log(coldWarmPushTest1);
+log(coldWarmPushTest2);
+
+logTitle("Array destructure");
+
+var seasons = ['winter', 'spring', 'summer', 'autumn'];
+var coldSeason, otherSeasons;
+
+coldSeason = seasons[0];
+otherSeasons = seasons.slice(1);
+
+
+log(seasons);
+log(coldSeason);
+log(otherSeasons);
+
+logTitle("Rest parameters -  with iterator example");
+//https://rainsoft.io/how-three-dots-changed-javascript/
+
+function iterator() {
+    var _this = this;
+
+    var index = 0;
+    return {
+        next: function next() {
+            return { // Conform to Iterator protocol
+                done: index >= _this.length,
+                value: _this[index++]
+            };
+        }
+    };
+}
+var arrayLike = {
+    0: 'Cat',
+    1: 'Bird',
+    2: 'Parrot',
+    3: 'Horse',
+    length: 4
+};
+
+arrayLike[_iterator3.default] = iterator; //Conform to Iterable Protocol
+var arrayFromObject = [].concat((0, _toConsumableArray3.default)(arrayLike));
+
+log(arrayLike);
+log(arrayFromObject);
 
 logTitle("Default parameters - strings example");
 
